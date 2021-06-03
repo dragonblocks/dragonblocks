42bd2832229a5b4a009611989c85184316b2bc6b
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-06-03 21:38:03 +0200

    Put every module into a separate line to avoid git conflicts when adding modules

--- engine/init.js
+++ engine/init.js
@@ -1,24 +1,24 @@
 /*
  * init.js
- * 
+ *
  * Copyright 2020 Elias Fleckenstein <eliasfleckenstein@web.de>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 $.ajaxSetup({
 	async: false,
@@ -56,7 +56,7 @@ if(dragonblocks.loggedin){
 dragonblocks.log = function(text){
 	console.log("[Dragonblocks] " + text);
 	dragonblocks.backlog += text + "\n";
-} 
+}
 dragonblocks.error = function(err){
 	let error = new Error(err);
 	dragonblocks.backlog += error;
@@ -161,7 +161,41 @@ dragonblocks.loadMod = function(modname){
 	dragonblocks.loadedMods.push(modname);
 	dragonblocks.loadingMods[modname] = false;
 }
-dragonblocks.modules = ["ressources", "key_handler", "gui", "mapgen", "world", "item", "node", "tool", "group", "builtin", "map_node", "map", "itemstack", "inventory", "inventory_group", "hudbar", "inventory_container", "creative_inventory", "recipe", "craftfield", "menu", "skin", "entity", "map_interaction", "spawned_entity", "item_entity", "falling_node", "timer", "player", "pixel_manipulator", "chat", "chatcommands", "mainmenu"];
+dragonblocks.modules = [
+	"ressources",
+	"key_handler",
+	"gui",
+	"mapgen",
+	"world",
+	"item",
+	"node",
+	"tool",
+	"group",
+	"builtin",
+	"map_node",
+	"map",
+	"itemstack",
+	"inventory",
+	"inventory_group",
+	"hudbar",
+	"inventory_container",
+	"creative_inventory",
+	"recipe",
+	"craftfield",
+	"menu",
+	"skin",
+	"entity",
+	"map_interaction",
+	"spawned_entity",
+	"item_entity",
+	"falling_node",
+	"timer",
+	"player",
+	"pixel_manipulator",
+	"chat",
+	"chatcommands",
+	"mainmenu",
+];
 dragonblocks.moduleCount = dragonblocks.modules.length;
 dragonblocks.loadModule = function(){
 	if(dragonblocks.modules[0]){
